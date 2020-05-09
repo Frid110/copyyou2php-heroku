334cfd526adc50393bab01adc8b74736aaf018fa
Frid110 <49295581+Frid110@users.noreply.github.com>
2020-05-09 12:34:42 +0800

    Add files via upload

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');//这里改成您的api key
+define('APIKEY', 'AIzaSyD8yTdQyboZEkGRuKOLwQoVB5xgYLiD1Qg');//这里改成您的api key
 define('GJ_CODE', 'NZ');//国家代码
 define('SITE_NAME', 'Share2018');//网站名字
 define('TITLENAME', '共享網飛群&遊管');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', 'wingwanwin@gmail.com');//邮箱
+define('EMAIL', 'nzd22225@gmail.com');//邮箱
 ?>
